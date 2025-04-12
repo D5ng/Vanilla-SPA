5a5ffabcdd13de7267538ccec8269cc158bfc53b
d5ng <dong5ffice@gmail.com>
2025-04-12 11:32:56 +0900

    🐛  Fix: VITE  환경 변수 에러 수정

--- src/service/weather.ts
+++ src/service/weather.ts
@@ -3,7 +3,7 @@ import { WeatherData } from "./weather.type"
 
 export async function fetchWeatherData() {
   const responseData = await fetcher<WeatherData>(
-    `https://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=${import.meta.env.WEATHER_API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=${import.meta.env.VITE_WEATHER_API_KEY}`,
   )
   return responseData
 }
