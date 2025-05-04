4474249a16f82bd9f109986f39d753e49aec369c
d5ng <dong5ffice@gmail.com>
2025-05-04 12:32:36 +0900

    ✨ Feat: add Weather Five day API

--- src/service/weather.ts
+++ src/service/weather.ts
@@ -1,9 +1,23 @@
 import { fetcher } from "../utils"
-import { WeatherData } from "./weather.type"
+import { WeatherData, WeatherFiveDaysType } from "./weather.type"
 
 export async function fetchWeatherData() {
   const responseData = await fetcher<WeatherData>(
-    `https://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=${import.meta.env.VITE_WEATHER_API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=${
+      import.meta.env.VITE_WEATHER_API_KEY
+    }&units=metric`,
   )
+  return responseData
+}
+
+export async function fetchWeather5dayData() {
+  const responseData = await fetcher<WeatherFiveDaysType>(
+    `https://api.openweathermap.org/data/2.5/forecast?q=Seoul,kr&appid=${
+      import.meta.env.VITE_WEATHER_API_KEY
+    }&units=metric`,
+  )
+
+  console.log(responseData)
+
   return responseData
 }
--- src/service/weather.type.ts
+++ src/service/weather.type.ts
@@ -1,36 +1,55 @@
+// 공통 타입
 export interface Coord {
   lon: number
   lat: number
 }
 
-export interface Weather {
+export interface WeatherDescription {
   id: number
   main: string
   description: string
   icon: string
 }
 
-export interface Main {
+export interface MainWeatherData {
   temp: number
   feels_like: number
   temp_min: number
   temp_max: number
   pressure: number
   humidity: number
-  sea_level: number
-  grnd_level: number
+  sea_level?: number
+  grnd_level?: number
+  temp_kf?: number
 }
 
 export interface Wind {
   speed: number
   deg: number
+  gust?: number
 }
 
 export interface Clouds {
   all: number
 }
 
-export interface Sys {
+export interface WeatherData {
+  coord: Coord
+  weather: WeatherDescription[]
+  base: string
+  main: MainWeatherData
+  visibility: number
+  wind: Wind
+  clouds: Clouds
+  dt: number
+  sys: SysInfo
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
+
+export interface SysInfo {
   type: number
   id: number
   country: string
@@ -38,18 +57,34 @@ export interface Sys {
   sunset: number
 }
 
-export interface WeatherData {
-  coord: Coord
-  weather: Weather[]
-  base: string
-  main: Main
-  visibility: number
-  wind: Wind
-  clouds: Clouds
+export interface WeatherFiveDaysType {
+  cod: string
+  message: number
+  cnt: number
+  list: WeatherFiveDaysItem[]
+  city: WeatherFiveDaysCity
+}
+
+export interface WeatherFiveDaysItem {
   dt: number
-  sys: Sys
-  timezone: number
+  main: MainWeatherData
+  weather: WeatherDescription[]
+  clouds: Clouds
+  wind: Wind
+  visibility: number
+  pop: number
+  rain?: { "3h": number }
+  sys: { pod: "d" | "n" }
+  dt_txt: string
+}
+
+export interface WeatherFiveDaysCity {
   id: number
   name: string
-  cod: number
+  coord: Coord
+  country: string
+  population: number
+  timezone: number
+  sunrise: number
+  sunset: number
 }
