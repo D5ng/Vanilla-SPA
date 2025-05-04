5893406444a7b06175b76782937a03d3d0fcb8c7
d5ng <dong5ffice@gmail.com>
2025-05-04 12:31:30 +0900

    ✨ Feat: update Nav Component

--- src/components/Nav.ts
+++ src/components/Nav.ts
@@ -4,13 +4,25 @@ import { Link } from "../lib/vanilla-router"
 export default class Nav extends Component {
   template(): string {
     return `
-      <nav>
-        <ul>
+      <nav class="p-2 flex justify-center bg-gray-200">
+        <ul class="flex gap-3 text-sm underline">
           <li>
             ${Link({ to: "/", children: "Home" })}
           </li>
           <li>
+            ${Link({ to: "/weather", children: "Weather" })}
+          </li>
+          <li>
+            ${Link({ to: "/weather/5day", children: "Weather 5days" })}
+          </li>
+          <li>
             ${Link({ to: "/about", children: "About" })}
+          </li>
+          <li>
+            ${Link({ to: "/about/detail", children: "AboutDetail" })}
+          </li>
+          <li>
+            ${Link({ to: "/guest", children: "Guest" })}
           </li>
         </ul>
       </nav>
